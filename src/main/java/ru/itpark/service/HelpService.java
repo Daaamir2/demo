57b6383eab2b925d7656af8ea5f4959f46482766
studen <student@localhost>
2018-11-16 13:08:45 +0300

    Initial

---
+++ src/main/java/ru/itpark/service/HelpService.java
@@ -0,0 +1,7 @@
+package ru.itpark.service;
+
+public class HelpService {
+    public String initials(String name){
+        return "";
+    }
+}
---
+++ src/test/java/ru/itpark/service/HelpServiceTest.java
@@ -0,0 +1,13 @@
+package ru.itpark.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelpServiceTest {
+
+    @Test
+    void initials() {
+
+    }
+}
